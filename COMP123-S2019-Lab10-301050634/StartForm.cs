ace938af2f4d9032b49b3469aba0f6d4f22cd160
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-07-17 11:43:17 -0400

    The Splash Page: Created the Tick event for the SplashTimer with the tasks and code needed.

--- COMP123-S2019-Lab10-301050634/StartForm.cs
+++ COMP123-S2019-Lab10-301050634/StartForm.cs
@@ -21,5 +21,13 @@ namespace COMP123_S2019_Lab10_301050634
         {
             SplashTimer.Enabled = true;
         }
+
+        private void SplashTimer_Tick(object sender, EventArgs e)
+        {
+            SplashTimer.Enabled = false;
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+            this.Hide();
+        }
     }
 }
